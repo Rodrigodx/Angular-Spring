282c8f8d876d6cf61e0fa642070dc71776ca8f08
Rodrigodx <rodrigodx52@gmail.com>
2024-02-09 11:33:33 -0300

    Form: Saving Data with HTTP POST + Error Handling

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
-import { delay, first, tap } from 'rxjs';
+import {first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +19,9 @@ export class CoursesService {
       first(),
       tap(courses => console.log(courses))
     );
+  }
+
+  save(record: Course){
+    return this.httpCLient.post<Course>(this.API, record).pipe(first());
   }
 }
